2b17c4f70ca11c61022b983353726135196658af
Eduardo Vázquez <60900038+eduvzb@users.noreply.github.com>
2021-01-20 11:32:24 -0600

    Se alimenta la base de datos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,18 +1,47 @@
 <?php
 
-namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
+
+use App\Models\ListaCarrera;
+use App\Models\ListaTramite;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $arrayCarreras = [
+            'Ing. En Sistemas Computacionales',
+            'Ing. Mecánica',
+            'Ing. Química',
+            'Ing. Bioquímica',
+            'Ing. Electrica',
+            'Ing. Electrónica',
+            'Ing. En Gestión Empresarial',
+            "Ing. Industrial",
+            'Ing. Logística'
+        ];
+
+        $arrayTramites =  [
+            'Programación de Examen Profesional',
+            'Constancia de Acreditación',
+            'Titulación'
+        ];
+
+        DB::table('lista_carreras')->delete();
+        DB::table('lista_tramites')->delete();
+
+        foreach ($arrayCarreras as $carrera)
+            ListaCarrera::create(['name' => $carrera]);
+        
+        foreach ($arrayTramites as $tramite)
+            ListaTramite::create(['name' => $tramite]);
+
+        User::create([
+            'email' => 'admin@app.com',
+            'password' => Hash::make("password"),
+            'is_admin' => 1
+        ]);
     }
 }
