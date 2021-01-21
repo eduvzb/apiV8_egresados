902bb5957af28c2d0f94cae86c0d3577f29c8837
Eduardo Vázquez <60900038+eduvzb@users.noreply.github.com>
2021-01-20 21:34:33 -0600

    reorganizando rutas

--- routes/web.php
+++ routes/web.php
@@ -29,52 +29,43 @@ Route::post('/register','App\Http\Controllers\Auth\RegisterController@postRegist
 
 Route::middleware('auth')->group( function (){
 
-    //Route::get('egresados','App\Http\Controllers\Admin\AdminController@allEgresados')->name('egresados.filtrar');
-
-    //Route::get('tramites','AdminController@allTramites')->name('tramite.filtrar');
-
-    //Route::get('citas','AdminController@allCitas')->name('citas.filtrar');
-
-    //Route::get('citasNoEnviadas','Citas\CitasController@getCitasNoEnviadas')->name('citas.noEnviadas');
-
-    
     Route::get('/register','App\Http\Controllers\Auth\RegisterController@getEmailRegister')->name('register.request');
     
     Route::post('/register/email','App\Http\Controllers\Auth\RegisterController@sendEmailRegister')->name('sendEmail.register');
 
-    Route::get('egresados','App\Http\Controllers\Admin\AdminController@allEgresados')->name('egresados.filtrar');
+    Route::get('egresados','App\Http\Controllers\Admin\EgresadosController@allEgresados')->name('egresados.filtrar');
 
-    Route::get('/egresados/{id}/delete','App\Http\Controllers\Admin\AdminController@deleteEgresado')->name('egresados.delete');
+    Route::get('/egresados/{id}/delete','App\Http\Controllers\Admin\EgresadosController@deleteEgresado')->name('egresados.delete');
     
-    Route::get('tramites','App\Http\Controllers\Admin\AdminController@allTramites')->name('tramite.filtrar');
+    Route::get('tramites','App\Http\Controllers\Admin\TramitesController@allTramites')->name('tramite.filtrar');
 
-    Route::get('/tramites/{id}/','App\Http\Controllers\Admin\AdminController@getTramites')->name('egresados.tramites');
+    Route::get('/tramites/{id}/','App\Http\Controllers\Admin\TramitesController@getTramites')->name('egresados.tramites');
 
-    Route::get('/tramite/{id}/finalizar','App\Http\Controllers\Admin\AdminController@finishTramite')->name('tramite.finish');
-
-    Route::get('/tramites/{id}/delete','App\Http\Controllers\Admin\AdminController@deleteTramite')->name('tramites.delete');
+    Route::get('/tramite/{id}/finalizar','App\Http\Controllers\Admin\TramitesController@finishTramite')->name('tramite.finish');
     
-    Route::get('citas','App\Http\Controllers\Admin\AdminController@allCitas')->name('citas.filtrar');
+    Route::get('/tramites/{id}/delete','App\Http\Controllers\Admin\TramitesController@deleteTramite')->name('tramites.delete');
     
-    Route::get('/citas/{id}/','App\Http\Controllers\Admin\AdminController@getCitas')->name('egresados.citas');
-
-    Route::post('/citas/{id}','App\Http\Controllers\Admin\AdminController@postCitar')->name('citar');
-
-    Route::get('/citas/{id}/delete','App\Http\Controllers\Admin\AdminController@deleteCita')->name('citas.delete');
-
-    Route::get('citasNoEnviadas','App\Http\Controllers\Admin\Citas\CitasController@getCitasNoEnviadas')->name('citas.noEnviadas');
+    Route::get('citas','App\Http\Controllers\Admin\CitasController@allCitas')->name('citas.filtrar');
     
-    Route::post('sendCitasPendientes','App\Http\Controllers\Admin\Citas\CitasController@sendMailsRestantes')->name('citas.enviarPendientes');
-
-    Route::get('crearTramite','App\Http\Controllers\Admin\AdminController@getCreateTramite')->name('listaTramites');
-
-    Route::post('createListaTramite','App\Http\Controllers\Admin\AdminController@storeTramite')->name('listaTramites.store');
+    Route::get('/citas/{id}/','App\Http\Controllers\Admin\CitasController@getCitas')->name('egresados.citas');
     
-    Route::get('deleteListaTramite/{id}/delete','App\Http\Controllers\Admin\AdminController@deleteListaTramite')->name('listraTramites.delete');
+    Route::post('/citas/{id}','App\Http\Controllers\Admin\CitasController@postCitar')->name('citar');
 
-    Route::get('editListaTramite/{id}/edit','App\Http\Controllers\Admin\AdminController@editListaTramite')->name('listraTramites.edit');
+    Route::get('/citas/{id}/delete','App\Http\Controllers\Admin\CitasController@deleteCita')->name('citas.delete');
+    
+    Route::get('citasNoEnviadas','App\Http\Controllers\Admin\CitasController@getCitasNoEnviadas')->name('citas.noEnviadas');
+    
+    Route::post('sendCitasPendientes','App\Http\Controllers\Admin\CitasController@sendMailsRestantes')->name('citas.enviarPendientes');
 
-    Route::patch('updateNameTramite/{id}/update','App\Http\Controllers\Admin\AdminController@updateNameTramite')->name('listraTramites.update');
+    Route::get('crearTramite','App\Http\Controllers\Admin\ListTramitesController@getCreateTramite')->name('listaTramites');
+
+    Route::post('createListaTramite','App\Http\Controllers\Admin\ListTramitesController@storeTramite')->name('listaTramites.store');
+    
+    Route::get('deleteListaTramite/{id}/delete','App\Http\Controllers\Admin\ListTramitesController@deleteListaTramite')->name('listraTramites.delete');
+
+    Route::get('editListaTramite/{id}/edit','App\Http\Controllers\Admin\ListTramitesController@editListaTramite')->name('listraTramites.edit');
+
+    Route::patch('updateNameTramite/{id}/update','App\Http\Controllers\Admin\ListTramitesController@updateNameTramite')->name('listraTramites.update');
 
     Route::get('downloadEgresados/{carrera}/{yearIngreso}/{yearEgreso}/{dateIngresoRange}/{dateEgresoRange}/{dateIngresoSpe}/{dateEgresoSpe}',[
         'as' => 'download.egresados',
@@ -95,12 +86,12 @@ Route::middleware('auth')->group( function (){
     Route::get('tramites_emails/{tramite}/{carrera}/{yearIngreso}/{yearEgreso}/
     {dateIngresoRange}/{dateEgresoRange}/{dateIngresoSpe}/{dateEgresoSpe}',[
         'as' => 'tramites_emails',
-        'uses' => 'App\Http\Controllers\Admin\AdminController@getMails'
+        'uses' => 'App\Http\Controllers\Admin\CitasController@getMails'
     ]);
 
     Route::post('sendEmails/{tramite}/{carrera}/{yearIngreso}/{yearEgreso}/
     {dateIngresoRange}/{dateEgresoRange}/{dateIngresoSpe}/{dateEgresoSpe}',[
         'as'   => 'citar.sendEmail',
-        'uses' => 'App\Http\Controllers\Admin\AdminController@sendEmails'
+        'uses' => 'App\Http\Controllers\Admin\CitasController@sendEmails'
     ]);
 });
