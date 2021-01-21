c6c87decc3b4578ed5a7e9d8ef6a8f684151de28
Eduardo Vázquez <60900038+eduvzb@users.noreply.github.com>
2021-01-20 21:33:36 -0600

    vista verificar modificada

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="content-wrapper">
+    <div class="content-header">
+        <div class="container col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">
+                    <h2 class="card-title">{{ __('Verificar Correo Electrónico') }}</h2>
+                </div>
 
                 <div class="card-body">
                     @if (session('resent'))
@@ -14,13 +16,13 @@
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('Antes de Proceder, por favor checa tu bandeja de email para verificar tu correo electrónico') }}
+                    {{ __('Si no recibiste el correo electrónico') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('Click aquí para solicitarlo otra vez') }}</button>.
                     </form>
-                </div>
+                </div>  
             </div>
         </div>
     </div>
