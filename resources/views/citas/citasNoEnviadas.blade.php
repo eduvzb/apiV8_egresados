495d3870bc3e2fcfebbacf8691675754261fca91
Eduardo Vázquez <60900038+eduvzb@users.noreply.github.com>
2021-01-22 12:52:41 -0600

    vista citas no enviadas modificadas

--- resources/views/citas/citasNoEnviadas.blade.php
+++ resources/views/citas/citasNoEnviadas.blade.php
@@ -4,75 +4,6 @@
 <div class="content-wrapper">
     <div class="content-header">
         <div class="container-fluid">
-
-        <!--
-        <div class="card " style="border:1px solid">
-            <div class="card-header">
-                <h2 class="card-title">Nueva Cita</h2>
-            </div>
-
-            <div class="card-body">
-                
-
-                    
-                    <div class="col-md-8 form-group">
-                        
-                        <div class="form-group">
-                            <label for="cita">Mensaje:</label>
-                            <textarea name="mensaje" id="mensaje" cols="8" rows="3" class="form-control @error('mensaje') is-invalid @enderror"></textarea>
-                            @error('mensaje')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-
-                        <div class="form-group">
-                            <label for="cita">Asunto:</label>
-                            <input name="asunto" id="asunto" type="text" class="form-control @error('asunto') is-invalid @enderror"></input>
-                            @error('mensaje')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-
-                        <div class="form-group">
-                            <label for="cita">Examinar Archivo:</label>
-                            <input name="file" class="form-control" id="formFile" type="file">
-                        </div>
-                        <a onclick="clearFile()" class="btn btn-warning">Borrar Archivo</a>  
-                    </div>
-
-                    <div class="col-md-4 form-group">
-                        <div class="form-group">
-                            <label for="cita">Fecha de cita:</label>
-                            <input id="fecha" type="date" class="form-control @error('fecha') is-invalid @enderror" name="fecha">
-                                @error('fecha')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                        </div>
-
-                        <div class="form-group">
-                            <label for="hora">Hora de cita:</label>
-                            <input id="hora" type="time" class="form-control @error('hora') is-invalid @enderror" name="hora">
-                                @error('hora')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{$message}}</strong>
-                                    </span>
-                                @enderror
-                        </div>
-
-                        <div class="text-center">
-                            <button class="btn btn-primary">Enviar Seleccionados</button>
-                            <a onclick="back()" class="btn btn-danger">Cancelar</a>
-                        </div>
-                    </div>
-            </div>
-        </div>
-        -->
         
         <div class="card">
                 <div class="card-header">
@@ -84,7 +15,7 @@
                     <div class="col-md-12 form-group text-right">
                         <div class="text-right">
                             <button class="btn btn-primary">Reenviar Seleccionados</button>
-                            <a onclick="back()" class="btn btn-danger">Cancelar</a>
+                            <a onclick="back()" class="btn btn-danger" style="color: white;">Cancelar</a>
                         </div>
                     </div>
                 </div>
