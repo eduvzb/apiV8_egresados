13ac88544378d3186bdba733b3c7cd95cce8f554
Eduardo Vázquez <60900038+eduvzb@users.noreply.github.com>
2021-01-21 23:03:57 -0600

    app blade modificada

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <div id="app">
         <div class="wrapper">
 
-            <nav class="main-header navbar navbar-expand navbar-light" style="background-color: #76A7F5;">
+            <nav class="main-header navbar navbar-expand-lg navbar-light bg-primary">
                 <ul class="navbar-nav">
                         <li class="nav-item">
                             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="material-icons">menu</i></a>
@@ -36,7 +36,7 @@
 
             <aside class="main-sidebar sidebar-dark-primary elevation-4">
                 <a href="{{ url('/') }}" class="brand-link">
-                <img src="assets/img/icon.png" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <img src="{{url('assets/img/icon.png')}}" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">ITTG</span>
                 </a>
 
