358b57078a68be2f2e5a1010d1052a22e06eb521
Eduardo Vázquez <60900038+eduvzb@users.noreply.github.com>
2021-01-22 00:13:31 -0600

    vista citas modificada

--- resources/views/citas/index.blade.php
+++ resources/views/citas/index.blade.php
@@ -3,70 +3,77 @@
 @section('content')
 <div class="content-wrapper">
     <div class="content-header">
+        <div class="container-fluid">
+            <div class="card">
+                <div class="card-header">
+                    <h2 class="card-title">Nueva Cita</h2>
+                </div>
+
+                <div class="card-body">
+                    <form class="row g-3" method="POST" enctype="multipart/form-data" action="{{route('citar',$id)}}">
+                    @csrf
+                        <div class="col-md-8 form-group">
+                            <div class="form-group">
+                                <label for="cita">Mensaje:</label>
+                                <textarea name="mensaje" id="mensaje" cols="8" rows="3" class="form-control @error('mensaje') is-invalid @enderror"></textarea>
+                                @error('mensaje')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label for="cita">Asunto:</label>
+                                <input name="asunto" id="asunto" type="text" class="form-control @error('asunto') is-invalid @enderror"></input>
+                                @error('asunto')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label for="cita">Examinar Archivo:</label>
+                                <input name="file" class="form-control" id="formFile" type="file">
+                            </div>
+                            <a onclick="clearFile()" class="btn btn-warning">Borrar Archivo</a>
+                        </div>
+
+                        <div class="col-md-4 form-group">
+                            <div class="form-group">
+                                <label for="cita">Fecha de cita:</label>
+                                <input id="fecha" type="date" class="form-control @error('fecha') is-invalid @enderror" name="fecha">
+                                    @error('fecha')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label for="hora">Hora de cita:</label>
+                                <input id="hora" type="time" class="form-control @error('hora') is-invalid @enderror" name="hora">
+                                    @error('hora')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{$message}}</strong>
+                                        </span>
+                                    @enderror
+                            </div>
+
+                            <div class="text-right">
+                                <button type="submit" class="btn btn-primary">Enviar</button>
+                                <a onclick="back()" class="btn btn-danger" style="color: white;">Cancelar</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        
         <div class="row justify-content-center">
             <div class="col-md-8">
-                    <div class="card" style="border:1px solid">
-                        <div class="card-header">
-                            <h2 class="card-title">Nueva Cita</h2>
-                        </div>
-
-                        <div class="card-body">
-                            <form class="row g-3" method="POST" enctype="multipart/form-data" action="{{route('citar',$id)}}">
-                            @csrf
-                                <div class="col-md-8">
-                                    <label for="cita">Mensaje:</label>
-                                    <textarea name="mensaje" id="mensaje" cols="8" rows="3" class="form-control @error('mensaje') is-invalid @enderror"></textarea>
-                                    @error('mensaje')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-
-                                    <label for="cita">Asunto:</label>
-                                    <input name="asunto" id="asunto" type="text" class="form-control @error('asunto') is-invalid @enderror"></input>
-                                    @error('asunto')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-
-                                <div class="col-md-4">
-                                    <label for="cita">Fecha de cita:</label>
-                                    <input id="fecha" type="date" class="form-control @error('fecha') is-invalid @enderror" name="fecha">
-                                        @error('fecha')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    <p></p>
-                                    <label for="hora">Hora de cita:</label>
-                                    <input id="hora" type="time" class="form-control @error('hora') is-invalid @enderror" name="hora">
-                                        @error('hora')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{$message}}</strong>
-                                            </span>
-                                        @enderror
-                                </div>
-
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col-md-8">
-                                            <label for="cita">Examinar Archivo:</label>
-                                            <input name="file" class="form-control" id="formFile" type="file">
-                                            <p></p>
-                                            <a onclick="clearFile()" class="btn btn-warning">Borrar Archivo</a>
-                                        </div>
-                                        
-                                        <div class="col align-self-end">
-                                            <button type="submit" class="btn btn-primary">Enviar</button>
-                                            <a onclick="back()" class="btn btn-danger btn-delete">Cancelar</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
 
