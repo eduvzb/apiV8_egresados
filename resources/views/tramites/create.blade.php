1bb03c65ba729fe93728eca4f2df590f077cadb1
Eduardo Vázquez <60900038+eduvzb@users.noreply.github.com>
2021-01-22 14:02:41 -0600

    responsive table tramites

--- resources/views/tramites/create.blade.php
+++ resources/views/tramites/create.blade.php
@@ -34,39 +34,41 @@
             </div>
         </div>
 
-        <div class="container col-8">
+        <div class="container col-md-8">
             <div class="card">
                 <div class="card-header">
                     <h2 class="card-title">Lista de Trámites Existentes</h2>
                 </div>
                 <div class="card-body">
-                    <table class="text-center table table-bordered table-striped">
-                        <thead>
-                            <tr>
-                                <th scope="col">Nombre del Trámite</th>
-                                <th scope="col">Fecha de Creación</th>
-                                <th scope="col" style="width: 180px">Opciones</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                        @foreach($tramites as $tramite)
-                            <tr>
-                                <td>{{$tramite->name}}</td>
-                                <td>{{$tramite->created_at}}</td>
-                                <td>
-                                    <a href="{{route('listraTramites.edit',$tramite->id)}}" class="btn btn-info btn-sm">
-                                            <abbr title="Editar Trámite"><i class="material-icons">edit</i></abbr>
-                                    </a>
-
-                                    <a onclick="confirmation(event)" href="{{route('listraTramites.delete',$tramite->id)}}" class="btn btn-danger btn-sm">
-                                            <abbr title="Eliminar Trámite"><i class="material-icons">delete</i></abbr>
-                                    </a>
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                    <div class="table-responsive">
+                        <table class="text-center table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Nombre del Trámite</th>
+                                    <th scope="col">Fecha de Creación</th>
+                                    <th scope="col" style="width: 180px">Opciones</th>
+                                </tr>
+                            </thead>
+    
+                            <tbody>
+                            @foreach($tramites as $tramite)
+                                <tr>
+                                    <td>{{$tramite->name}}</td>
+                                    <td>{{$tramite->created_at}}</td>
+                                    <td>
+                                        <a href="{{route('listraTramites.edit',$tramite->id)}}" class="btn btn-info btn-sm">
+                                                <abbr title="Editar Trámite"><i class="material-icons">edit</i></abbr>
+                                        </a>
+    
+                                        <a onclick="confirmation(event)" href="{{route('listraTramites.delete',$tramite->id)}}" class="btn btn-danger btn-sm">
+                                                <abbr title="Eliminar Trámite"><i class="material-icons">delete</i></abbr>
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
